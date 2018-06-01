fa6518932102e42b52af1783b81181bc564700f6
vinicius-marcondes <viniciusmarc_@hotmail.com>
2018-06-01 06:23:34 -0300

    host's ip configuration

--- WebSite/pages/banco.php
+++ WebSite/pages/banco.php
@@ -1,5 +1,5 @@
 ﻿<?php
-	$conecta_banco = new mysqli('localhost', 'root', 'usbw', 'mdi');
+	$conecta_banco = new mysqli('192.168.100.69', 'vinicius', 'admin', 'mdi');
 	if (!$conecta_banco) {
     die('Could not connect: ' . mysql_error());
 	}
