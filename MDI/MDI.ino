4fc03bc82a331cd62557314fe906f19d3a236033
Vinicius-Marcondes <viniciusmarc_@hotmail.com>
2018-06-03 01:54:09 -0300

    Now arduino con read the name of the user

--- MDI/MDI.ino
+++ MDI/MDI.ino
@@ -19,39 +19,57 @@ MMA8452Q accel;
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };//mac do arduino
 char ID [64];
 char URL[128];
+char NOME[128];
 char X[16];
 char Y[16];
-char c;
+char c;//infrações
+char i;//lê os valores do nome do funcionário
 char CONT;
 int ID_ARDUINO = 1;
 int t = 0;
 float f;
 float b;
 float delta;
-IPAddress server(191,232,52,179); //ip da internet
+String string;//Armazena o nome completo do funcionário
+
+IPAddress server(191,239,243,126); //ip da internet
 IPAddress ip(192, 168, 100, 60); //ip do arduino
 EthernetClient client;
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Waiting for Serial!");
+  while(!Serial);
   Ethernet.begin(mac, ip);
   accel.init();
   delay(5000);
-  Serial.println("connecting...");
+  Serial.println("Connecting to server");
   while (!client.connect(server, 8080));
+  Serial.println("Connected");
   sprintf(ID, "GET /cont.php?ID_ARDUINO=%d", ID_ARDUINO);
   client.println(ID);
   delay(2000);
-  c = client.read();
-  CONT = c - 48;
+  c = client.read();//recebe nomero de infrações
+  CONT = c - 48;//converte c em inteiro;
   client.println("Connection: close");
-  Serial.println("Connected!");
+  client.stop();  
+  while (!client.connect(server, 8080));
+  sprintf(NOME, "GET /cont.php?ID_ARDUINO=%d", ID_ARDUINO);
+  client.println(NOME);
+  client.println();
   delay(2000);
-  client.stop();
+  while(client.available()){//loop para pegar o nome do funcionário
+    i = client.read(); 
+    string = string+i;
+    client.println("Connection: keep-alive");
+    delay(500); 
+  }//fecha o loop
+  Serial.println(string);
+  client.println("Connection: close");
+  client.stop();  
 }
 
 void loop() {
-  delay(100);
   if (accel.available()) {
     accel.read();
     printCalculatedAccels();
@@ -64,7 +82,7 @@ void loop() {
       t = 0;
     }
     delta = (f - b);
-    if (accel.cx > 1 || accel.cx < -1 || delta > 0.7 || accel.cy >1 || accel.cy <-1) {
+    if (accel.cx > 1 || delta > 0.7 || delta < -0.7|| accel.cy >1 || accel.cy <-1) {
       while (!client.connect(server, 8080));
       float VALOR_X = accel.cx;
       float VALOR_Y = accel.cy;
