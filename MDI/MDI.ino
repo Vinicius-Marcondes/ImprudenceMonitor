6716e958c145e5691a09cd74020134f2588bf5c3
Vinicius-Marcondes <viniciusmarc_@hotmail.com>
2018-09-11 09:27:17 -0300

    EEPROM OK, LEDs OK

--- MDI/MDI.ino
+++ MDI/MDI.ino
@@ -17,6 +17,8 @@
 #include <LiquidCrystal.h>
 #include <SparkFun_MMA8452Q.h>
 
+#define green A1
+#define red A0
 #define port 80
 #define pinoutput 3
 #define ID_ARDUINO  1
@@ -25,7 +27,7 @@ MMA8452Q accel;
 
 EthernetClient client;
 LiquidCrystal lcd(4, 5, 9, 8, 7, 6);
-IPAddress ip(192, 168, 100, 70); //ip do arduino
+IPAddress ip(192, 168, 100, 149); //ip do arduino
 IPAddress server(191, 232, 196, 80); //ip da internet
 
 struct userObj {
@@ -34,33 +36,45 @@ struct userObj {
   char Y[6];
   String userId;
 };
-
 char URL[64];
+bool net;
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-userObj userData = {0, 0, 0, 0};
+
+userObj userData = {
+  8,
+  "1.25",
+  "-0.5",
+  "1"
+};
 
 void setup() {
   pinMode(pinoutput, OUTPUT);
-  analogWrite(pinoutput, 100);
+  analogWrite(pinoutput, 98);
+  pinMode(green, OUTPUT);
+  digitalWrite(green, 1);
+  pinMode(red, OUTPUT);
+  digitalWrite(red, 0);  
 
   Serial.begin(9600);
   lcd.begin(16, 2);
 
-  Serial.println("aaaah");
-  while (!client.connect(server, port));
-  Serial.println("eeee");
-
   accel.init(SCALE_8G, ODR_6);
   Ethernet.begin(mac, ip);
+
+  conn();
+  client.print("Connection: close");
+  client.stop();
 
   lcd.print("Initializing");
 
   Serial.println("GETTING USERID AND CONT");
-  userData.CONT = getCont();
-  userData.userId = getUserId();
+  Serial.println(net);
+  //printEeprom();
+  readEeprom();
+  sendData(userData.X, userData.Y, userData.CONT);
+  getCont();
+  getUserId();
   printLCD();
-  Serial.println(userData.userId);
-  Serial.println(userData.CONT);
   delay(1000);
 }
 
@@ -68,28 +82,19 @@ void loop() {
   accel.read();
   printCalculatedAccels();
   if (accel.cx > 1) {
+    digitalWrite(green,0);
     tone(2, 440);
+    digitalWrite(red,1);
     delay(500);
     noTone(2);
     userData.CONT++;
     printLCD();
     dtostrf(accel.cx, 1, 2, userData.X);
     dtostrf(accel.cy, 1, 2, userData.Y);
-    sprintf(URL, "GET /update.php?ID_ARDUINO=%d&CONT=%d&VALOR_X=%s&VALOR_Y=%s", ID_ARDUINO, userData.CONT, userData.X, userData.Y);
-    if (sendData(URL)) {
-      return true;
-    }
-    else {
-      printEeprom();
-      delay(500);
-      Serial.print("\nEEPROM: ");
-      readEeprom();
-      Serial.println(userData.CONT);
-      Serial.println(userData.userId);
-      Serial.println(userData.X);
-      Serial.println(userData.Y);
-    }
+    sendData(userData.X, userData.Y, userData.CONT);
+    digitalWrite(red,0);
     delay(5000);
+    digitalWrite(green,1);
   }
   Serial.println();
 }
@@ -104,16 +109,40 @@ void readEeprom() {
 
 bool conn() {
   if (client.connect(server, port)) {
+    net = true;
     return true;
   }
   else {
+    net = false;
     return false;
   }
 }
 
-String getUserId() {
-  String userid;
+bool sendData(char X[6], char Y[6], int CONT) {
+  sprintf(URL, "GET /update.php?ID_ARDUINO=%d&CONT=%d&VALOR_X=%s&VALOR_Y=%s", ID_ARDUINO, CONT, X, Y);
+  Serial.println(URL);
   if (conn()) {
+    client.println(URL);
+    client.print("Connection: close");
+    client.stop();
+    return true;
+  }
+  else {
+    printEeprom();
+    delay(500);
+    Serial.print("\nEEPROM: ");
+    readEeprom();
+    Serial.println(userData.CONT);
+    Serial.println(userData.userId);
+    Serial.println(userData.X);
+    Serial.println(userData.Y);
+    return false;
+  }
+}
+
+void getUserId() {
+  if (conn()) {
+    String userid;
     sprintf(URL, "GET /userID.php?ID_ARDUINO=%d", ID_ARDUINO);
     client.println(URL);
     client.println("Connection: close");
@@ -124,8 +153,11 @@ String getUserId() {
       userid = userid + c;
     }
     client.stop();
+    userData.userId = userid;
   }
-  return userid;
+  else {
+    //readEeprom();
+  }
 }
 
 int getCont() {
@@ -143,7 +175,10 @@ int getCont() {
     Serial.println(CONT);
     client.println("Connection: close");
     client.stop();
-    return CONT;
+    userData.CONT = CONT;
+  }
+  else {
+    //readEeprom();
   }
 }
 
@@ -156,6 +191,10 @@ void printCalculatedAccels() {
 
 void printLCD() {
   lcd.clear();
+  lcd.setCursor(13, 0);
+  lcd.print("E=");
+  lcd.setCursor(15, 0);
+  lcd.print(net);
   lcd.setCursor(0, 0);
   lcd.print("User ID: ");
   lcd.setCursor(8, 0);
@@ -166,15 +205,3 @@ void printLCD() {
   lcd.print(userData.CONT);
 }
 
-bool sendData(String var) {
-  Serial.println(var);
-  if (conn()) {
-    client.println(var);
-    client.print("Connection: close");
-    client.stop();
-    return true;
-  }
-  else {
-    return false;
-  }
-}
